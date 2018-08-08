492c9be939b591ec5963a198b46263dc586841cc
Soloria Wesp <gckain@mail.ru>
2018-08-08 21:08:17 +0300

    add abstract and  main pages

---
+++ WebDrive/Pages/AbstractPage.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WebDrive
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+
+    public abstract class AbstractPage
+    {
+        private readonly IWebDriver driver;
+
+        protected AbstractPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public IWebDriver getDriver() => driver;
+    }
+}
---
+++ WebDrive/Pages/MainPage.cs
@@ -0,0 +1,44 @@
+﻿
+namespace WebDrive
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    using JetBrains.Annotations;
+
+    public class MainPage : AbstractPage
+    {
+        public MainPage(IWebDriver driver) : base(driver) {}
+
+        [FindsBy(How = How.Name, Using = "userName")]
+        private IWebElement NameField { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement PasswordField { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "login")]
+        private IWebElement LoginButton { get; [UsedImplicitly] set; }
+
+        public MainPage InputName(string name)
+        {
+            NameField.Click();
+            NameField.Clear();
+            NameField.SendKeys(name);
+            return this;
+        }
+
+        public MainPage InputPassword(string password)
+        {
+            PasswordField.Click();
+            PasswordField.Clear();
+            PasswordField.SendKeys(password);
+            return this;
+        }
+
+        public MainPage ClickLoginButton()
+        {
+            LoginButton.Click();
+            return this;
+        }
+    }
+    
+}
