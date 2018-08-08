9aed77173abd6a9d6788219c8a9e522ccfbde9e1
Soloria Wesp <gckain@mail.ru>
2018-08-09 01:56:39 +0300

    add flight finder page

--- WebDrive/Pages/FlightFinderPage.cs
+++ WebDrive/Pages/FlightFinderPage.cs
@@ -3,6 +3,7 @@
 namespace WebDrive
 {
     using System.Linq;
+    using System.Runtime.CompilerServices;
     using OpenQA.Selenium;
     using SeleniumExtras.PageObjects;
     using JetBrains.Annotations;
@@ -13,6 +14,9 @@ namespace WebDrive
 
         [FindsBy(How = How.TagName, Using ="img")]
         private IWebElement HeaderImage { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.CssSelector, Using ="(input[type='radio'][value='oneway'])")]
+        private IWebElement OneWayTripType { get; [UsedImplicitly] set; }
 
         public string HeaderText => HeaderImage.GetAttribute("src").Split("/").LastOrDefault();
     }
