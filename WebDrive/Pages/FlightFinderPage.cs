53c9b5e6846551e0f644ddc2b3e5e799184d4a10
Soloria Wesp <gckain@mail.ru>
2018-08-09 14:19:16 +0300

    add select flight page

--- WebDrive/Pages/FlightFinderPage.cs
+++ WebDrive/Pages/FlightFinderPage.cs
@@ -3,7 +3,6 @@
 namespace WebDrive
 {
     using System.Linq;
-    using System.Runtime.CompilerServices;
     using OpenQA.Selenium;
     using SeleniumExtras.PageObjects;
     using JetBrains.Annotations;
@@ -17,6 +16,30 @@ namespace WebDrive
 
         [FindsBy(How = How.CssSelector, Using ="(input[type='radio'][value='oneway'])")]
         private IWebElement OneWayTripType { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "passCount")]
+        private IWebElement PassengersCount { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "fromPort")]
+        private IWebElement DepartingFrom { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "fromMonth")]
+        private IWebElement Month { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "fromDay")]
+        private IWebElement Day { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "toPort")]
+        private IWebElement ArrivingIn { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.CssSelector, Using ="(input[type='radio'][value='Business'")]
+        private IWebElement BusinessServiceClass { get; [UsedImplicitly] set; }
+
+        [FindsBy (How = How.Name, Using ="airline")]
+        private IWebElement Airline { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using ="findFlights")]
+        private IWebElement ContinueButton { get; [UsedImplicitly] set; }
 
         public string HeaderText => HeaderImage.GetAttribute("src").Split("/").LastOrDefault();
     }
---
+++ WebDrive/Pages/SelectFlightPage.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace WebDrive
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    using JetBrains.Annotations;
+
+    public class SelectFlightPage : AbstractPage
+    {
+       public SelectFlightPage(IWebDriver driver) : base(driver) {}
+
+        [FindsBy(How = How.TagName, Using ="img")]
+        private IWebElement HeaderImage { get; [UsedImplicitly] set; }
+
+
+    }
+}
