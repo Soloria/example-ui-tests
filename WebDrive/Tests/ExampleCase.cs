02edfb2a3c90aaa70d8bae5812bc19ffd1d934c3
Soloria Wesp <gckain@mail.ru>
2018-08-10 01:30:54 +0300

    add login test

---
+++ WebDrive/Tests/ExampleCase.cs
@@ -0,0 +1,30 @@
+﻿namespace WebDrive
+{
+    using NUnit.Framework;
+
+    public class ExampleCase
+    {
+        [OneTimeSetUp]
+        public static void Start()
+        {
+            SessionManager.Open(SessionManager.Path);
+        }
+
+        [Test]
+        [Order(order: 1)]
+        public void LogIn()
+        {
+            new MainPage(SessionManager.getDriver())
+                .InputName("")
+                .InputPassword("")
+                .ClickLoginButton();
+            Assert.IsTrue(new FlightFinderPage(SessionManager.getDriver()).FlightFinderHeader.Displayed);
+        }
+
+        [OneTimeTearDown]
+        public static void Clean()
+        {
+            SessionManager.Close();
+        }
+    }
+}
