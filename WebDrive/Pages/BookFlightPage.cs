143ac9fbea3ab918690330a4abc0e72866dd456d
Soloria Wesp <gckain@mail.ru>
2018-08-09 16:01:57 +0300

    add book a  flight page

---
+++ WebDrive/Pages/BookFlightPage.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace WebDrive
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    using JetBrains.Annotations;
+
+    public class BookFlightPage : AbstractPage
+    {
+        public BookFlightPage(IWebDriver driver) : base(driver) {}
+
+        [FindsBy(How = How.Name, Using = "passFirst0")]
+        private IWebElement FirstNameField { get; [UsedImplicitly] set; }
+    }
+}
--- WebDrive/Pages/SelectFlightPage.cs
+++ WebDrive/Pages/SelectFlightPage.cs
@@ -2,6 +2,7 @@
 
 namespace WebDrive
 {
+    using System.Linq;
     using OpenQA.Selenium;
     using SeleniumExtras.PageObjects;
     using JetBrains.Annotations;
@@ -13,6 +14,15 @@ namespace WebDrive
         [FindsBy(How = How.TagName, Using ="img")]
         private IWebElement HeaderImage { get; [UsedImplicitly] set; }
 
+        [FindsBy(How = How.CssSelector, Using ="(input[name='outFlight'][value^='Unified Airlines$361'])")]
+        private IWebElement OutUnifiedAirlines363 { get; [UsedImplicitly] set; }
 
+        [FindsBy(How = How.CssSelector, Using ="(input[name='inFlight'][value^='Blue Skies Airlines$631])")]
+        private IWebElement InBlueSkiesAirlines631 { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using ="reserveFlights")]
+        private IWebElement ContinueButton { get; [UsedImplicitly] set; }
+
+        public string HeaderText => HeaderImage.GetAttribute("src").Split("/").LastOrDefault();
     }
 }
