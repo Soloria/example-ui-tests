8c1c6d33b5176b66e8155633d2006bf1d80bd72c
Soloria Wesp <gckain@mail.ru>
2018-08-12 00:33:24 +0300

    add sign on page

---
+++ WebDrive/Pages/SignOnPage.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace WebDrive
+{
+    using OpenQA.Selenium;
+    using SeleniumExtras.PageObjects;
+    using JetBrains.Annotations;
+
+    public class SignOnPage : AbstractPage
+    {
+        public SignOnPage(IWebDriver driver) : base(driver) {}
+
+        [FindsBy(How = How.CssSelector, Using = "img[src*='mast_signon']")]
+        private IWebElement SignOnHeader { get; [UsedImplicitly] set; }
+    }
+}
