c7a53b48ec54f69f85b7846ddfb2d11e18f54928
Soloria Wesp <gckain@mail.ru>
2018-08-11 17:43:39 +0300

    update flight finder page

--- WebDrive/Pages/FlightFinderPage.cs
+++ WebDrive/Pages/FlightFinderPage.cs
@@ -5,6 +5,7 @@ namespace WebDrive
     using OpenQA.Selenium;
     using SeleniumExtras.PageObjects;
     using JetBrains.Annotations;
+    using OpenQA.Selenium.Support.UI;
 
     public class FlightFinderPage : AbstractPage
     {
@@ -17,16 +18,16 @@ namespace WebDrive
         private IWebElement OneWayTripType { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using = "passCount")]
-        private IWebElement PassengersCount { get; [UsedImplicitly] set; }
+        private IWebElement Passengers { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using = "fromPort")]
         private IWebElement DepartingFrom { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using = "fromMonth")]
-        private IWebElement Month { get; [UsedImplicitly] set; }
+        private IWebElement Months { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using = "fromDay")]
-        private IWebElement Day { get; [UsedImplicitly] set; }
+        private IWebElement Days { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using = "toPort")]
         private IWebElement ArrivingIn { get; [UsedImplicitly] set; }
@@ -35,12 +36,70 @@ namespace WebDrive
         private IWebElement BusinessServiceClass { get; [UsedImplicitly] set; }
 
         [FindsBy (How = How.Name, Using ="airline")]
-        private IWebElement Airline { get; [UsedImplicitly] set; }
+        private IWebElement Airlines { get; [UsedImplicitly] set; }
 
         [FindsBy(How = How.Name, Using ="findFlights")]
         private IWebElement ContinueButton { get; [UsedImplicitly] set; }
 
-       
 
+        public FlightFinderPage ChooseOnewWayTripType()
+        {
+            OneWayTripType.Click();
+            return this;
+        }
+
+        public FlightFinderPage ChoosePassengersCount(string number)
+        {
+            var PassengersCount = new SelectElement(Passengers);
+            PassengersCount.SelectByValue(number);
+            return this;
+        }
+
+        public FlightFinderPage ChooseDepartingFrom(string port)
+        {
+            var Port = new SelectElement(DepartingFrom);
+            Port.SelectByValue(port);
+            return this;
+        }
+
+        public FlightFinderPage ChooseMonth(string month)
+        {
+            var Month = new SelectElement(Months);
+            Month.SelectByValue(month);
+            return this;
+        }
+
+        public FlightFinderPage ChooseDay(string day)
+        {
+            var Day = new SelectElement(Days);
+            Day.SelectByValue(day);
+            return this;
+        }
+
+        public FlightFinderPage ChooseArrivingIn(string port)
+        {
+            var Port = new SelectElement(ArrivingIn);
+            Port.SelectByValue(port);
+            return this;
+        }
+
+        public FlightFinderPage ChooseBusinessClass()
+        {
+            BusinessServiceClass.Click();
+            return this;
+        }
+
+        public FlightFinderPage ChooseAirline(string airline)
+        {
+            var Airline = new SelectElement(Airlines);
+            Airline.SelectByValue(airline);
+            return this;
+        }
+
+        public BookFlightPage ClickContinueButton()
+        {
+            ContinueButton.Click();
+            return new BookFlightPage(SessionManager.getDriver());
+        }
     }
 }
