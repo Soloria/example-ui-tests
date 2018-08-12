de4e667d3daf86b4d17a1edc36eafd2708fc7bc4
Soloria Wesp <gckain@mail.ru>
2018-08-12 03:14:19 +0300

    update sign on page

--- WebDrive/Pages/SignOnPage.cs
+++ WebDrive/Pages/SignOnPage.cs
@@ -12,5 +12,34 @@ namespace WebDrive
 
         [FindsBy(How = How.CssSelector, Using = "img[src*='mast_signon']")]
         private IWebElement SignOnHeader { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "userName")]
+        private IWebElement Username { get; [UsedImplicitly] set; }
+        
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement Password { get; [UsedImplicitly] set; }
+
+        [FindsBy(How = How.Name, Using = "login")]
+        private IWebElement SubmitButton { get; [UsedImplicitly] set; }
+
+        public SignOnPage InputUsername(string name)
+        {
+            Username.Click();
+            Username.SendKeys(name);
+            return this;
+        }
+
+        public SignOnPage InputPassword(string pass)
+        {
+            Password.Click();
+            Password.SendKeys(pass);
+            return this;
+        }
+
+        public FlightFinderPage ClickSubmitButton()
+        {
+            SubmitButton.Click();
+            return new FlightFinderPage(SessionManager.getDriver());
+        }
     }
 }
