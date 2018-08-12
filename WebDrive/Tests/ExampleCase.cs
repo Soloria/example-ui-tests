bcf37b5f1a31e5f02713e9a358da72f7f923b958
Soloria Wesp <gckain@mail.ru>
2018-08-13 00:07:56 +0300

    add enter flight details test

--- WebDrive/Tests/ExampleCase.cs
+++ WebDrive/Tests/ExampleCase.cs
@@ -19,6 +19,25 @@
             Assert.IsTrue(new FlightFinderPage(SessionManager.getDriver()).FlightFinderHeader.Displayed);
         }
 
+        [Test]
+        [Order(order: 2)]
+        public void EnterFlightDetails()
+        {
+            new FlightFinderPage(SessionManager.getDriver())
+                .ChooseOneWayTripType()
+                .ChoosePassengersCount("2")
+                .ChooseDepartingFrom("Paris")
+                .ChooseMonthOn("11")
+                .ChooseDayOn("20")
+                .ChooseArrivingIn("Seattle")
+                .ChooseReturningMonth("12")
+                .ChooseReturningDay("19")
+                .ChooseBusinessClass()
+                .ChooseAirline("Pangea Airlines")
+                .ClickContinueButton();
+            Assert.IsTrue(new SelectFlightPage(SessionManager.getDriver()).HeaderBookAFlight.Displayed);
+        }
+
         [OneTimeTearDown]
         public static void Clean() => SessionManager.Close();
     }
